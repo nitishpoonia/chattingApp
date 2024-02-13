0b97b8bb6af7f46620de387f6b0d4c6eea3f68c8
Nitish Poonia <npoonia39@gmail.com>
2024-02-13 12:04:32 +0530

    Added firebase, login, register, logout

---
+++ Flash Chat iOS13/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Flash Chat iOS13
+//
+//  Created by Nitish Poonia on 13/02/24.
+//  Copyright © 2024 Angela Yu. All rights reserved.
+//
+
+struct Constants{
+    static let registerSegue = "RegisterToChat"
+    static let loginSegue = "LoginToChat"
+}
--- Flash Chat iOS13/Controllers/RegisterViewController.swift
+++ Flash Chat iOS13/Controllers/RegisterViewController.swift
@@ -7,13 +7,25 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class RegisterViewController: UIViewController {
 
+    @IBOutlet weak var errorLabel: UILabel!
     @IBOutlet weak var emailTextfield: UITextField!
     @IBOutlet weak var passwordTextfield: UITextField!
     
     @IBAction func registerPressed(_ sender: UIButton) {
+        
+        if let email = emailTextfield.text, let password = passwordTextfield.text{
+            Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+                if let e = error{
+                    self.errorLabel.text = e.localizedDescription
+                } else {
+                    self.performSegue(withIdentifier: Constants.registerSegue, sender: self)
+                }
+            }
+        }
     }
     
 }
